2bed54607c697000d5ce2f75fa897963787d80ed
Tianyu Li <ltyucb@gmail.com>
2020-02-11 23:25:29 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
-
+#include <vulkan/vulkan.h>
+#include <spdlog/spdlog.h>
+#include <glm/glm.hpp>
+#include <assimp/Importer.hpp>
 
 int main()
 {
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Vulkan";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    VkInstance instance;
+
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+    if (result == VK_SUCCESS)
+    {
+        spdlog::info("Vulkan Instance Created!");
+    }
+
     return 0;
 }
